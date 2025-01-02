dd5042a2911b203a613cbb09db4c959ee63bbf9e
Moshe Maman <moshe.maman@gmail.com>
2025-01-02 19:30:47 +0000

    port 80

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
     });
 });
 
-const PORT = 3000;
+const PORT = 80;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
