3956fc14da9a37dde161abd923f472c02299edcf
Moshe Maman <moshe.maman@gmail.com>
2025-02-18 21:47:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const http = require('http');
+export default {
+  async fetch(request, env) {
+    return await handleRequest(request)
+  }
+}
 
-const server = http.createServer((req, res) => {fetch('https://moshema8.wixsite.com/ppctools/_functions/headlines')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+async function handleRequest(request) {
+  if (request.method === "POST") {
+    const payload = await request.json() 
+
+    const url = "https://moshema8.wixstudio.com/speak-pt/_functions/telegramHook"
+    const data = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Content-Type": "application/json"
       }
-      return response.json();
-    })
-    .then(data => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data.html);
-    }).catch(err => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-    })
-});
-
-const PORT = 80;
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    }) 
+    
+  }
+  return new Response("OK") // Doesn't really matter
+}
